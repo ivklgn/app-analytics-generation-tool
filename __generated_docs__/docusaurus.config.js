cc2117a1f4b4acaf6acf65cc6902af8fc7f2b1e2
Kalagin Ivan <lulldev@yandex.ru>
2024-07-09 00:13:54 +0500

    feat: add search for docusaurus

--- __generated_docs__/docusaurus.config.js
+++ __generated_docs__/docusaurus.config.js
@@ -8,6 +8,7 @@ import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
+  plugins: [require.resolve("@cmfcmf/docusaurus-search-local")],
   title: "Analytics Docs",
   // tagline: "Dinosaurs are cool",
   favicon: "img/favicon.ico",
